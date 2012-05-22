3777f7428a498bf870203f44e64f96071966af81
Ilkka Laukkanen <ilkka.s.laukkanen@gmail.com>
2012-05-22 07:05:29 +0300

    Quick test of play todo tutorial app

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -2,11 +2,37 @@ package controllers
 
 import play.api._
 import play.api.mvc._
+import play.api.data._
+import play.api.data.Forms._
 
 object Application extends Controller {
   
-  def index = Action {
-    Ok(views.html.index("Your new application is ready."))
-  }
-  
+	val taskForm = Form(
+		"label" -> nonEmptyText
+	)
+
+  	def index = Action {
+    	//Ok(views.html.index("Your new application is ready."))
+    	Redirect(routes.Application.tasks)
+  	}
+
+	def tasks = Action {
+		Ok(views.html.index(models.Task.all(), taskForm))
+	}
+
+	def newTask = Action { implicit request =>
+		taskForm.bindFromRequest.fold(
+			errors => BadRequest(views.html.index(models.Task.all(), errors)),
+			label => {
+				models.Task.create(label)
+				Redirect(routes.Application.tasks)
+			}
+		)
+	}
+
+	def deleteTask(id: Long) = Action {
+		models.Task.delete(id)
+		Redirect(routes.Application.tasks)
+	}
+
 }
