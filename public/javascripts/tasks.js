a88febecba32d32a7e72dc26c40065cd5852301c
Ilkka Laukkanen <ilkka.s.laukkanen@gmail.com>
2012-05-30 09:23:16 +0300

    Add function for creating new tasks to view model
    It also saves the task data immediately

--- public/javascripts/tasks.js
+++ public/javascripts/tasks.js
@@ -6,6 +6,17 @@ function Task(data) {
 function TasksViewModel() {
 	var self = this;
 	self.tasks = ko.observableArray([]);
+	self.newTaskText = ko.observable("");
+
+	self.create = function() {
+		self.tasks.push(new Task({ label: self.newTaskText() }));
+		self.newTaskText("");
+		 $.ajax("/tasks", {
+		 	data: ko.toJSON({ tasks: self.tasks }),
+		 	type: "post", contentType: "application/json",
+		 	success: function(result), { /* it'd be nice to flash a message here */ }
+		 });
+	}
 
 	$.getJSON("/tasks", function(data) {
 		var mapped = $.map(data, function(item) { console.log(item); return new Task(item); });
